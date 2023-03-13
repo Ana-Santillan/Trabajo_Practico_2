495813d009f3173994fc3775d6db3802b5eb091c
Ana Santillan <anisantillan10@gmail.com>
2023-03-13 19:07:25 -0300

    Se agrego el ejercicio 17 que muestra la posicion de la primera vocal y se modifico la variable text por texto en el ejercicio 15

--- js/ejercicio15.js
+++ js/ejercicio15.js
@@ -2,15 +2,15 @@
 Realiza un script que cuente el número de vocales que tiene un texto.
 */
 
-const text = prompt("Ingrese un texto");
+const texto = prompt("Ingrese un texto");
 
 let contador = 0;
 
-for (let i = 0; i < text.length; i++) {
-    const letra = text.charAt(i);
+for (let i = 0; i < texto.length; i++) {
+    const letra = texto.charAt(i);
     if ("aeiouAEIOU".includes(letra)) {
         contador ++;
     }
 }
 
-document.write(`La cantidad de vocales en la frase "${text}" es: ${contador}`);
+document.write(`La cantidad de vocales en la frase "${texto}" es: ${contador}`);
